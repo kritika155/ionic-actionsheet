bf419c215133a0bef5fa5160480184fa184499cf
kritika roy <roykritika00@gmail.com>
2019-05-09 04:15:54 +0530

    actionsheet

---
+++ ionicactionsheet/src/app/home/home.page.ts
@@ -0,0 +1,57 @@
+//home.page.ts
+import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  actionSheet:any;
+
+  constructor(public actionSheetController: ActionSheetController) {}
+
+  presentActionSheet() {
+    this.actionSheet = this.actionSheetController.create({
+      header: 'Freaky Jolly',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'arrow-dropright-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    }).then(actionsheet => {
+      actionsheet.present();
+    });
+  }
+
+
+}
